2666e27836d45fc2f46eeb61ea166a20817723da
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-05-16 13:55:22 +0700

    membuat router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {CommonModule} from '@angular/common';
 import {ReactiveFormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {GeneralComponent} from './home/general/general.component';
+import {ApplicationComponent} from './home/application/application.component';
+
+const routerApp: Routes = [
+  {
+    path: 'home', component: HomeComponent, children: [
+      {path: 'general', component: GeneralComponent},
+      {path: 'application', component: ApplicationComponent},
+    ]
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GeneralComponent,
+    ApplicationComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(routerApp)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
